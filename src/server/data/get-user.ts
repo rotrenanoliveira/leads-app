81bf688a3e3bf0a6302e883fedb1ba420c2329e0
Renan Oliveira <rotrenanoliveira@gmail.com>
2025-02-06 11:15:54 -0300

    feat: add find user by id

--- src/server/data/get-user.ts
+++ src/server/data/get-user.ts
@@ -1,6 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { handle } from '@/utils/functions'
-import type { ResponseError, User } from '@/utils/types'
+import { type ResponseError, type User, type UserInfo, userInfoSchema } from '@/utils/types'
 
 export async function getUser(email: string): Promise<[User | null, null] | [null, ResponseError]> {
   const result = await handle(
@@ -12,4 +12,22 @@ export async function getUser(email: string): Promise<[User | null, null] | [nul
   )
 
   return result
+}
+
+export async function getUserById(userId: string): Promise<[UserInfo | null, null] | [null, ResponseError]> {
+  const [user, queryError] = await handle(
+    prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    }),
+  )
+
+  if (queryError) return [null, queryError]
+
+  if (!user) return [null, null]
+
+  const userInfo = userInfoSchema.parse(user)
+
+  return [userInfo, null]
 }
