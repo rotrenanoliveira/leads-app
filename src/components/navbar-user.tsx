96a8aeb2f34174f655c82c1c856c597575a4eea9
Renan Oliveira <rotrenanoliveira@gmail.com>
2025-02-11 11:33:36 -0300

    feat: redirect to login if user not found

--- src/components/navbar-user.tsx
+++ src/components/navbar-user.tsx
@@ -3,13 +3,14 @@ import { ChevronsUpDown, Settings2Icon } from 'lucide-react'
 import { NavbarUserDropdown } from './navbar-user-dropdown'
 import { getUserById } from '@/server/data/get-user'
 import { getCurrentUserId } from '@/server/data/get-current-user'
+import { redirect } from 'next/navigation'
 
 export async function NavbarUser() {
   const userId = await getCurrentUserId()
   if (!userId) return null
 
   const userResult = await getUserById(userId.value)
-  if (userResult[0] === null) throw userResult[1]
+  if (userResult[0] === null) redirect('/login')
 
   const user = userResult[0]
 
